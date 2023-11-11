adf200a596afe09724b000006649b315187f33d1
taseen12 <elantaseen@gmail.com>
2023-11-11 23:34:34 +0600

    eighth fina commit

--- script.js
+++ script.js
@@ -15,18 +15,6 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     We got your order of (${quantity}) pieces of (${guitar}) guitar . 
     After checking  we will get back to you in your given email (${email}) `);
 });
-function show() {
-
-    var person = document.getElementById("nameInput").value ;
-
-    var courses = document.getElementById("courseInput").value; 
-    
-    
-    document.getElementById("mainContent").innerText = "Hello " +  person  + " nice to know that you have taken " + courses + " courses"; 
-    
-
-
-}
 
 
 
